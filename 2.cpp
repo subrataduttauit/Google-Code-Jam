b6da9266cbabcf32f2f9c9b8304b1f5a8e1b497d
subrataduttauit <subrataduttauit@gmail.com>
2015-09-08 23:01:03 +0530

    solution

---
+++ 2.cpp
@@ -0,0 +1,46 @@
+/* 
+ * Solution to Google Code Jam Problems
+ * by Subrata Dutta
+ * 
+ * University Institute of Technology, Burdwan University
+ * Dept. of Information Technology
+ * https://github.com/subrataduttauit
+ * https://in.linkedin.com/in/subrataduttauit
+ */
+
+
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int T,N,C[1000];
+    
+    scanf("%d",&T);
+    
+    for(int tc = 1;tc <= T;++tc){
+        scanf("%d",&N);
+        
+        int aux = 0;
+        
+        for(int i = 0;i < N;++i){
+            scanf("%d",&C[i]);
+            aux ^= C[i];
+        }
+        
+        printf("Case #%d: ",tc);
+        
+        if(aux != 0) puts("NO");
+        else{
+            long long ans = 0;
+            sort(C,C+N);
+            
+            for(int i = 1;i < N;++i) ans += C[i];
+            
+            printf("%lld\n",ans);
+        }
+    }
+    
+    return 0;
+}
